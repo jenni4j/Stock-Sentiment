0420e90502893b542bc0518d69f068dc71bf5f74
Jennifer Jordache <jenniferjordache@Jennifers-MacBook-Pro.local>
2022-10-05 19:40:16 -0700

    completed sentiment

--- scraper.py
+++ scraper.py
@@ -1,19 +1,30 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from bs4 import BeautifulSoup as soup
 from urllib.request import Request, urlopen
+import nltk
+nltk.downloader.download('vader_lexicon')
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
-# Input
-symbol = input('Enter a ticker: ')
-print('Getting data for ' + symbol + '...\n')
 
-# Set up scraper
-url = ("http://finviz.com/quote.ashx?t=" + symbol.lower())
-req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-webpage = urlopen(req).read()
-html = soup(webpage, "html.parser")
+def main():
+    news = get_news()
+    scores = get_sentiment(news)
+    plot(scores)
+
 
 def get_news():
+    # Input
+    symbol = input('Enter a ticker: ')
+    print('Getting data for ' + symbol + '...\n')
+
+    # Set up scraper
+    url = ("http://finviz.com/quote.ashx?t=" + symbol.lower())
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    webpage = urlopen(req).read()
+    html = soup(webpage, "html.parser")
+
     try:
         # Find news table
         news = pd.read_html(str(html), attrs={'class': 'fullview-news-outer'})[0]
@@ -22,13 +33,27 @@ def get_news():
             links.append(a['href'])
 
         # Clean up news dataframe
-        news.columns = ['Date', 'News Headline']
-        news['Article Link'] = links
-        news = news.set_index('Date')
-        print(news)
+        news.columns = ['date', 'headline']
+        news['link'] = links
+        news['date'] = pd.to_datetime(news['date'], dayfirst=True)
+        news['date'] = [d.date() for d in news['date']]
         return news
 
     except Exception as e:
         return e
 
-print(get_news())
+
+def get_sentiment(news):
+    vader = SentimentIntensityAnalyzer()
+    news['compound'] = news['headline'].apply(lambda score: vader.polarity_scores(score)['compound'])
+    news['sentiment'] = news['compound'].apply(lambda c: 'pos' if c > 0 else('neg' if c < 0 else 'neutral'))
+    return news
+
+
+def plot(scores):
+    scores['sentiment'].value_counts().plot(kind='bar')
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
